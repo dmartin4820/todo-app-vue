151ca57cd2e0d22bb9b5e4aead4975f7e5059dd6
Dmartin <dom4822@yahoo.com>
2021-09-15 21:53:12 -0700

    finish completed component: add completedItem component and modify props passed to completed

--- src/components/Completed/Completed.vue
+++ src/components/Completed/Completed.vue
@@ -1,22 +1,25 @@
 <template>
-  <h2>Completed Todos</h2>
-  <ul>
-    <CompletedItem v-for="item in completedList" v-bind:item="item" v-bind:key="item.id"/>
-  </ul>
+  <div id="list">
+    <h2>Completed Todos</h2>
+    <ul>
+      <CompletedItem 
+        v-for="item in completedList" 
+        v-bind:item="item"
+        v-bind:key="item.id"/>
+    </ul>
+  </div>
 </template>
 
 <script>
   import CompletedItem from "./CompletedItem.vue";
 
   export default {
-    data() {
-      return {
-        completedList: []
-      }
-    },
+    props: ['completedList'], 
     components: {
       CompletedItem
     },
-    props: ['completedItem']
   }
 </script>
+
+<style>
+</style>
---
+++ src/components/Completed/CompletedItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <li id="completedItem">
+    <div id="completedContainer">
+      <p id="completedContent">{{ item.content }}</p>
+    </div>
+  </li>
+</template>
+
+<script>
+  export default {
+    props: ['item']
+  }
+</script>
+
+<style>
+  #completedItem {
+    list-style-type: none;
+  }
+  #completedContainer {
+    display: flex;
+    justify-content: center;
+  }
+</style>
