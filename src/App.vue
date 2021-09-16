0997743911a5355a4cdba7dec9c78006cf3b13f8
Dmartin <dom4822@yahoo.com>
2021-09-15 21:56:20 -0700

    create todoList, completedList, and handleCompleted as globals and modify props passed to components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <Header/>
-  <TodoList/>
-  <CompletedList/>
+  <input v-model="todoInput"/>
+  <button v-on:click="handleAdd">Add</button>
+  <div id="container">
+    <TodoList v-bind:todoList="todoList" v-bind:handleCompleted="handleCompleted"/>
+    <CompletedList v-bind:completedList="completedList"/>
+  </div>
 </template>
 
 <script>
@@ -11,6 +15,29 @@ import CompletedList from './components/Completed/Completed.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      todoInput: '',
+      todoList: [],
+      completedList: [],
+    }
+  },
+  methods: {
+    handleAdd() {
+      this.todoList.push({id: this.todoList.length, content: this.todoInput});
+      this.todoInput = '';
+    },
+    handleCompleted(e) {
+      this.todoList = this.todoList.filter((item) => {
+        if (item.id === Number(e.target.id)) {
+          this.completedList.push(item);
+          return false;
+        } else {
+          return true;
+        }
+      })
+    }
+  },
   components: {
     Header,
     TodoList,
@@ -20,12 +47,33 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
+  #app {
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #container {
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  #list {
+    border-radius: 5%;
+    border-style: solid;
+    border-width: 1px;
+    width: 400px;
+    height: 800px;
+    padding: 20px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './examples/app_example_bind.vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
